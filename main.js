55a8f19e1085943286eb33d632b7576241f8143f
Rondel_Hallare <hallarerondel@yahoo.com>
2019-09-08 11:55:26 +0800

    add file elective

---
+++ main.js
@@ -0,0 +1,10 @@
+let firstName = document.getElementById('firstName');
+let secondName = document.getElementById('#firstName').value;
+
+function getInput(){
+
+    let input= document.querySelector('#firstName').value;
+    let outPut = document.querySelector('#output');
+    console.log(input);
+    output.setAttribute('value', input);
+}
